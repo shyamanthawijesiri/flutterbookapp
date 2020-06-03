896b2bfbadbfb9bd12a9f482b298ce904c541913
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-03 16:24:59 +0530

    add drawer

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
-class ProductsPage extends StatelessWidget{
+
+class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return  Scaffold(
-        appBar: AppBar(
-          title: Text('Hello'),
-        ),
-        body: ProductManager(),
-      );
+    return Scaffold(
+      drawer: Drawer(
+          child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Choose'),
+          ),
+          ListTile(
+            title: Text('Manage Product'),
+            onTap: () {},
+          )
+        ],
+      )),
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: ProductManager(),
+    );
   }
 }
