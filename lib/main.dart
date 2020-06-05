a33e646a002e21730d09e57740cc3292c5afc857
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-05 14:42:40 +0530

    unknownroute

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,17 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp>{
-    List<Map<String,String>> _product = [];
+class _MyAppState extends State<MyApp> {
+  List<Map<String, String>> _product = [];
 
-      void _addProduct(Map<String, String> product){
-     setState(() {
-                _product.add(product);
-      });
-      print(_product);
+  void _addProduct(Map<String, String> product) {
+    setState(() {
+      _product.add(product);
+    });
+    print(_product);
   }
 
-  void _deleteProduct(int index){
+  void _deleteProduct(int index) {
     setState(() {
       _product.removeAt(index);
     });
@@ -36,33 +36,37 @@ class _MyAppState extends State<MyApp>{
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple
-      ),
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       //home:AuthPage(),
       routes: {
-        '/' : (BuildContext context) => ProductsPage(_product, _addProduct, _deleteProduct),
-        '/admin' : (BuildContext context) => ProductsAdiminPage(),
+        '/': (BuildContext context) =>
+            ProductsPage(_product, _addProduct, _deleteProduct),
+        '/admin': (BuildContext context) => ProductsAdiminPage(),
       },
 
-      onGenerateRoute: (RouteSettings settings){
+      onGenerateRoute: (RouteSettings settings) {
         final List<String> pathElements = settings.name.split('/');
-        if(pathElements[0] != ''){
+        if (pathElements[0] != '') {
           return null;
         }
-        if(pathElements[1] == 'product'){
+        if (pathElements[1] == 'product') {
           final int index = int.parse(pathElements[2]);
-          return    MaterialPageRoute<bool>(
-                    builder: (BuildContext context) => ProductPage(
-                        _product[index]['title'], _product[index]['image']),
-                  );
+          return MaterialPageRoute<bool>(
+            builder: (BuildContext context) =>
+                ProductPage(_product[index]['title'], _product[index]['image']),
+          );
         }
         return null;
-        
-        
       },
 
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (BuildContext context) =>
+              ProductsPage(_product, _addProduct, _deleteProduct),
+        );
+      },
     );
   }
 }
--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -24,7 +24,7 @@ ProductsPage(this.product, this.addProduct, this.deleteProduct);
           ListTile(
             title: Text('Manage Product'),
             onTap: () {
-                Navigator.pushReplacementNamed(context, '/admin');
+                Navigator.pushReplacementNamed(context, '/admins');
             },
           )
         ],
