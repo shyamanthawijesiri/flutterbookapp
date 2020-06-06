15bd70d8473e30b7f1f3aebc8b259f22998b5836
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-06 17:31:54 +0530

    add input field

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,16 +1,34 @@
-
+// -----------------------------Modal----------------------------
+// Center(child: RaisedButton(child: Text('save'), 
+//     onPressed: () {
+//       showModalBottomSheet(context: context, builder: (BuildContext context){
+//         return Center(child: Text('This is modal'),);
+//       });
+//     },),);
 
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatefulWidget{
+
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreatePageState();
+  }
+}
+class _ProductCreatePageState extends State<ProductCreatePage>{
+String titleValue = '';
+
 @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(child: RaisedButton(child: Text('save'), 
-    onPressed: () {
-      showModalBottomSheet(context: context, builder: (BuildContext context){
-        return Center(child: Text('This is modal'),);
-      });
-    },),);
+    return Column(children:<Widget>[
+      TextField(onChanged: (String value){
+        setState(() {
+          titleValue = value;
+        });
+      },),
+      Text(titleValue),
+    ]);
   }
 }
