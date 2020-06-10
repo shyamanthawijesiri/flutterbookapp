0200732bcd231a3f28ccae38e3770e37b99b21d2
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-10 18:30:05 +0530

    media query

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -18,87 +18,92 @@ class _AuthPageState extends State<AuthPage> {
   String _password;
   bool _acceptTerms = false;
 
-DecorationImage _buildBackgroundImage(){
-  return  DecorationImage(
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5), BlendMode.dstATop),
-              image: AssetImage('assets/leaves.jpg'),);
-}
+  DecorationImage _buildBackgroundImage() {
+    return DecorationImage(
+      fit: BoxFit.cover,
+      colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+      image: AssetImage('assets/leaves.jpg'),
+    );
+  }
 
-Widget _buildEmailTextField(){
-  return  TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                onChanged: (String value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-              );
-}
+  Widget _buildEmailTextField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
 
-Widget _buildPasswordTextField(){
-  return   TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                onChanged: (String value) {
-                  setState(() {
-                    _password = value;
-                  });
-                },
-              );
-}
+  Widget _buildPasswordTextField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _password = value;
+        });
+      },
+    );
+  }
 
-Widget _buildAcceptSwitch(){
-  return SwitchListTile(
-                  value: _acceptTerms,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _acceptTerms = value;
-                    });
-                  },
-                  title: Text('Acept terms'));
-}
+  Widget _buildAcceptSwitch() {
+    return SwitchListTile(
+        value: _acceptTerms,
+        onChanged: (bool value) {
+          setState(() {
+            _acceptTerms = value;
+          });
+        },
+        title: Text('Acept terms'));
+  }
 
-void _submitForm(){
-  Navigator.pushReplacementNamed(context, '/products');
-}
+  void _submitForm() {
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0: deviceWidth*0.95;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sample Code'),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          image: _buildBackgroundImage()
-          ),
-        
+        decoration: BoxDecoration(image: _buildBackgroundImage()),
         padding: EdgeInsets.all(10.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-            children: <Widget>[
-              _buildEmailTextField(),
-             
-              SizedBox(height: 10.0),
-              _buildPasswordTextField(),
-              _buildAcceptSwitch(),
-              SizedBox(height: 10.0),
-              RaisedButton(child: Text('Login'), onPressed: _submitForm)
-            ],
-          ),),
+            child: Container(
+              width: targetWidth,
+              child: Column(
+                children: <Widget>[
+                  _buildEmailTextField(),
+                  SizedBox(height: 10.0),
+                  _buildPasswordTextField(),
+                  _buildAcceptSwitch(),
+                  SizedBox(height: 10.0),
+                  RaisedButton(child: Text('Login'), onPressed: _submitForm)
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
